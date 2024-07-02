d0a4e779daf12a832b138cfb71dd93826ddbf80f
Lloyd <jolloyd@deloitte.co.uk>
2024-07-02 12:27:52 +0100

    initial commit

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,43 @@
+<div id="menu" class="pure-menu pure-menu-horizontal" role="navigation">
+  <a href="#" class="pure-menu-heading pure-menu-link"><i class="fa-solid fa-book-atlas"></i></a>
+  <ul class="pure-menu-list">
+    <li class="pure-menu-item">
+      <a href="#" class="pure-menu-link">News</a>
+    </li>
+    <li class="pure-menu-item">
+      <a href="#" class="pure-menu-link">Sports</a>
+    </li>
+    <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+      <a href="#" id="menuLink1" class="pure-menu-link">Train</a>
+      <ul class="pure-menu-children">
+        <li class="pure-menu-item">
+            <a href="#" class="pure-menu-link"></a>
+        </li>
+        <li class="pure-menu-item">
+            <a href="#" class="pure-menu-link">Twitter</a>
+        </li>
+        <li class="pure-menu-item">
+            <a href="#" class="pure-menu-link">Tumblr Blog</a>
+        </li>
+      </ul>
+    </li>
+    <li class="pure-menu-item">
+      <a href="#" class="pure-menu-link"><img class="flag-icon" src="/ita.png" alt=""></a>
+    </li>
+  </ul>
+</div>
+
+<style>
+  #menu {
+    background-color: var(--colour3);
+  }
+
+  #menu a {
+    color: black;
+  }
+  
+  .flag-icon {
+    width: 24px;
+    height: 24px;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "purecss";
+  import "$lib/global.css";
+  import Menu from "$lib/components/Menu.svelte";
+</script>
+
+<main>
+  <Menu />
+  <slot></slot>
+</main>
